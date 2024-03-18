9f8e1812f28bd3539122c03b09c18268368b05b1
DASW Project <project.dasw@gmail.com>
2024-03-18 11:07:10 +0700

    update index.php

--- index.php
+++ index.php
@@ -7,11 +7,79 @@
         <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
         <link rel="manifest" href="favicon/site.webmanifest">
+        
+        <!-- FONT STYLE -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Saira:ital,wght@0,100..900;1,100..900&display=swap"
+            rel="stylesheet">
+        <!-- FONT STYLE -->
+
+        <!-- FONT AWESOME ICON -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/fontawesome.min.css"
+            integrity="sha512-d0olNN35C6VLiulAobxYHZiXJmq+vl+BGIgAxQtD5+kqudro/xNMvv2yIHAciGHpExsIbKX3iLg+0B6d0k4+ZA=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+            integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/brands.min.css"
+            integrity="sha512-8RxmFOVaKQe/xtg6lbscU9DU0IRhURWEuiI0tXevv+lXbAHfkpamD4VKFQRto9WgfOJDwOZ74c/s9Yesv3VvIQ=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/solid.css"
+            integrity="sha512-o4nUMQptICEY+GgMk9oWsOOM8LV/Gvq+N/AJgZ2LIAR7r9wzTZ+MIJpPVjv4S0FVnpeBplQrxfIXKqVxHQzw8g=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/regular.min.css"
+            integrity="sha512-TzeemgHmrSO404wTLeBd76DmPp5TjWY/f2SyZC6/3LsutDYMVYfOx2uh894kr0j9UM6x39LFHKTeLn99iz378A=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- FONT AWESOME ICON -->
+
+        <link rel="stylesheet" href="assets/style.css">
         <title>Inventaris Gereja</title>
     </head>
     <body>
-        <div class="landing-header">
-            
-        </div>
+        <section class="landing-header">
+            <div class="header-logo">
+                <img src="img/logo-paroki-new.png" alt="image">
+                <div class="header-logo-caption">
+                    <h2>Gereja Katolik Paroki St. Albertus de Trapani</h2>
+                    <a href="https://parokiblimbing.org/" target="_blank">
+                        parokiblimbing.org
+                    </a>
+                </div>
+            </div>
+            <nav class="header-menu-nav">
+                <ul>
+                    <li>
+                        <a href="">
+                            <i class="fa-solid fa-house"></i>
+                            <span>Beranda</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa-regular fa-folder-open"></i>
+                            <span>Katalog Barang</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa-solid fa-people-group"></i>
+                            <span>Tentang</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa-solid fa-right-to-bracket"></i>
+                            <span>Login</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </section>
+        <section class="landing-body">
+            <div class="landing-body-heading">
+                <h2>Beranda</h2>
+            </div>
+        </section>
     </body>
 </html>
