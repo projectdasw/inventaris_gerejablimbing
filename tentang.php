a14fb589d50d308120ca49553a38ea47b8d96221
DASW Project <project.dasw@gmail.com>
2024-03-22 01:16:02 +0700

    update tentang.php

---
+++ tentang.php
@@ -0,0 +1,34 @@
+<div class="landing-about">
+    <h2>Aplikasi Inventaris Barang Gereja Katolik Paroki St. Albertus de Trapani</h2>
+    <p>
+        Aplikasi Inventaris Barang Gereja Katolik Paroki St. Albertus de Trapani
+        adalah aplikasi berbasis website yang bertujuan untuk mengelola data inventaris
+        barang milik Gereja Katolik Paroki St. Albertus de Trapani.
+    </p>
+    <p>
+        Aplikasi ini telah dirancang untuk pengelolaan sekaligus pendataan barang milik
+        gereja dari yang terlama hingga barang yang terbaru. Aplikasi ini di rancang
+        oleh 2 orang mahasiswa Teknik Informatika dari Universitas Bina Nusantara yaitu
+        Ronald Alvaro Kusuma dan Kenny Jonathan Setiosaputro.
+    </p>
+    <p>
+        Aplikasi ini telah di update sistemnya beserta tampilannya
+        oleh Ketua Komunikasi Sosial Albertus Broadcast & Media
+        (ARCADIA) Damianus Dimas Agil Satrio Wibowo.
+    </p>
+    <div class="landing-developer">
+        <div class="card-container">
+            <div class="card">
+                <div class="card-img">
+                    
+                </div>
+                <div class="card-name">
+
+                </div>
+                <div class="card-testimon">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
