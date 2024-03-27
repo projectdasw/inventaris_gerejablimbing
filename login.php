0f23fd36622ab0636c96838eb1e0596f35928dea
DASW Project <project.dasw@gmail.com>
2024-03-27 14:29:27 +0700

    update login.php

---
+++ login.php
@@ -0,0 +1,56 @@
+<div class="login-form">
+    <fieldset>
+        <legend>Login - Inventaris Gereja</legend>
+        <form action="" class="form-container" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="">
+                    <i class="fa-solid fa-circle-user"></i>
+                    Username
+                </label>
+                <input type="text" placeholder="Masukkan Username Anda">
+            </div>
+            <div class="form-group">
+                <label for="">
+                    <i class="fa-solid fa-key"></i>
+                    Password
+                </label>
+                <input type="password" placeholder="Masukkan Password Anda">
+            </div>
+            <div class="button-group">
+                <button type="submit" class="btn-small btn-success">
+                    <i class="fa-solid fa-right-to-bracket"></i>
+                    Login
+                </button>
+                <a href="">Lupa Password?</a>
+            </div>
+        </form>
+    </fieldset>
+    <div class="info-sidebar">
+        <div class="info-img">
+            <img src="img/logo-paroki-new.webp" alt="image">
+        </div>
+        <div class="info-desc">
+            <h2>Gereja Katolik Paroki St. Albertus de Trapani</h2>
+            <div class="soc-med">
+                <a href="https://www.youtube.com/@parokiblimbing" target="_blank">
+                    <i class="fa-brands fa-youtube" style="color: red;"></i>
+                    <span>Paroki Santo Albertus de Trapani</span>
+                </a>
+                <a href="https://www.instagram.com/parokiblimbing/" target="_blank">
+                    <i class="fa-brands fa-instagram" style="color: #ff5900;"></i>
+                    <span>@parokiblimbing</span>
+                </a>
+                <a href="https://parokiblimbing.org/" target="_blank">
+                    <i class="fa-solid fa-globe" style="color: blue;"></i>
+                    <span>www.parokiblimbing.org</span>
+                </a>
+            </div>
+            <address>
+                Jl. Jend A Yani 22,
+                Blimbing,
+                Kota Malang,
+                Jawa Timur 65126
+            </address>
+        </div>
+    </div>
+</div>
