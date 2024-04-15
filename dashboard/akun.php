8b405799fc90453dfd62101b5a7fc2ece8bca44f
DASW Project <project.dasw@gmail.com>
2024-04-15 15:35:39 +0700

    tambah file akun.php

---
+++ dashboard/akun.php
@@ -0,0 +1,25 @@
+<table id="table-data" class="display" style="width:100%">
+    <thead>
+        <tr>
+            <th>ID Akun</th>
+            <th>Nama Akun</th>
+            <th>Username</th>
+            <th>Password</th>
+            <th>Level Akun</th>
+            <th>No. HP</th>
+        </tr>
+    </thead>
+    <tbody>
+        
+    </tbody>
+    <tfoot>
+        <tr>
+            <th>ID Akun</th>
+            <th>Nama Akun</th>
+            <th>Username</th>
+            <th>Password</th>
+            <th>Level Akun</th>
+            <th>No. HP</th>
+        </tr>
+    </tfoot>
+</table>
