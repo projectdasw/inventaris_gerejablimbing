2307bf037c669398b6007dcb63efa20af8076db9
DASW Project <project.dasw@gmail.com>
2024-04-15 15:35:27 +0700

    tambah file aktivitas.php

---
+++ dashboard/aktivitas.php
@@ -0,0 +1,19 @@
+<table id="table-data" class="display" style="width:100%">
+    <thead>
+        <tr>
+            <th>Waktu/Tanggal</th>
+            <th>User/Akun</th>
+            <th>Aktivitas</th>
+        </tr>
+    </thead>
+    <tbody>
+        
+    </tbody>
+    <tfoot>
+        <tr>
+            <th>Waktu/Tanggal</th>
+            <th>User/Akun</th>
+            <th>Aktivitas</th>
+        </tr>
+    </tfoot>
+</table>
