dac6a72f489f1a422ab1f226685b009caa365a34
DASW Project <project.dasw@gmail.com>
2024-04-15 15:37:33 +0700

    tambah file laporan.php

---
+++ dashboard/laporan.php
@@ -0,0 +1,59 @@
+<div class="report-heading">
+    <div class="heading">
+        <h1>Data Total Harta Benda Gereja</h1>
+        <span>Gereja Katolik Paroki St. Albertus de Trapani</span>
+    </div>
+    <div class="count-all">
+        <h3>Total Harta Benda Gereja</h3>
+        <span>9999</span>
+    </div>
+</div>
+<table id="table-data" class="display" style="width:100%">
+    <thead>
+        <tr>
+            <th>Bangunan</th>
+            <th>Total Barang</th>
+        </tr>
+    </thead>
+    <tbody>
+
+    </tbody>
+    <tfoot>
+        <tr>
+            <th>Bangunan</th>
+            <th>Total Barang</th>
+        </tr>
+    </tfoot>
+</table>
+<table id="table_trans_barang" class="display" style="width:100%">
+    <h1>Detail Harta Benda</h1>
+    <thead>
+        <tr>
+            <th>No. Trans</th>
+            <th>ID Barang</th>
+            <th>Nama Barang</th>
+            <th>Jumlah</th>
+            <th>Deskripsi</th>
+            <th>Bangunan</th>
+            <th>Lokasi Utama</th>
+            <th>Lokasi Sekunder</th>
+            <th>Status</th>
+        </tr>
+    </thead>
+    <tbody>
+
+    </tbody>
+    <tfoot>
+        <tr>
+            <th>No. Trans</th>
+            <th>ID Barang</th>
+            <th>Nama Barang</th>
+            <th>Jumlah</th>
+            <th>Deskripsi</th>
+            <th>Bangunan</th>
+            <th>Lokasi Utama</th>
+            <th>Lokasi Sekunder</th>
+            <th>Status</th>
+        </tr>
+    </tfoot>
+</table>
