e3b8729b70625ec0dae707dba847f85556c6ca02
DASW Project <project.dasw@gmail.com>
2024-04-15 15:37:44 +0700

    tambah file list-data.php

---
+++ dashboard/list-data.php
@@ -0,0 +1,31 @@
+<table id="table-data" class="display" style="width:100%">
+    <thead>
+        <tr>
+            <th>ID Barang</th>
+            <th>Nama Barang</th>
+            <th>Jumlah</th>
+            <th>Kondisi</th>
+            <th>Keterangan</th>
+            <th>Bangunan</th>
+            <th>Lokasi Utama</th>
+            <th>Lokasi Sekunder</th>
+            <th>Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        
+    </tbody>
+    <tfoot>
+        <tr>
+            <th>ID Barang</th>
+            <th>Nama Barang</th>
+            <th>Jumlah</th>
+            <th>Kondisi</th>
+            <th>Keterangan</th>
+            <th>Bangunan</th>
+            <th>Lokasi Utama</th>
+            <th>Lokasi Sekunder</th>
+            <th>Aksi</th>
+        </tr>
+    </tfoot>
+</table>
