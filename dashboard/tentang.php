2e11591b847962eb0de6593de2d012ce02a29586
DASW Project <project.dasw@gmail.com>
2024-10-11 10:27:38 +0700

    file baru halaman tentang

---
+++ dashboard/tentang.php
@@ -0,0 +1,78 @@
+<div class="container-fluid d-flex flex-column mb-4 p-0">
+    <p class="card-text">
+        Aplikasi Inventaris Barang Gereja Katolik Paroki St. Albertus de Trapani
+        adalah aplikasi berbasis website yang bertujuan untuk mengelola data inventaris
+        barang milik Gereja Katolik Paroki St. Albertus de Trapani.
+    </p>
+    <p class="card-text">
+        Aplikasi ini dirancang untuk pengelolaan sekaligus pendataan barang milik
+        gereja dari yang terlama hingga barang yang terbaru. Aplikasi ini di rancang
+        oleh 2 orang mahasiswa Teknik Informatika dari Universitas Bina Nusantara yaitu
+        Ronald Alvaro Kusuma dan Kenny Jonathan Setiosaputro.
+    </p>
+    <p class="card-text">
+        Aplikasi ini telah di update sistemnya beserta tampilannya
+        oleh Ketua Komunikasi Sosial Paroki Albertus Broadcast & Media
+        (ARCADIA) Damianus Dimas Agil Satrio Wibowo.
+    </p>
+</div>
+<div class="container-fluid">
+    <div class="row justify-content-around">
+        <div class="col-12 col-md-5 col-lg-4 col-xl-4 col-xxl-3 card mb-1" style="
+            background-image: url('../img/card-name.webp'); background-size: cover;">
+            <img src="../img/ronald.webp" class="img-custom p-2" alt="image">
+            <div class="card-body bg-white">
+            <figure class="m-0">
+                <blockquote class="blockquote m-0 fs-6">
+                    <p>
+                        Terima Kasih sudah memberikan kami kesempatan
+                        untuk melakukan skripsi di Gereja St. Albertus De Trapani Malang.
+                    </p>
+                </blockquote>
+                <figcaption class="blockquote-footer m-0">
+                    Ronald Alvaro Kusuma,
+                    Mahasiswa Teknik Infromatika Universitas Bina Nusantara Malang
+                </figcaption>
+            </figure>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 col-xl-4 col-xxl-3 card mb-1" style="
+            background-image: url('../img/card-name.webp'); background-size: cover;">
+            <img src="../img/kenny.webp" class="img-custom p-2" alt="image">
+            <div class="card-body bg-white">
+            <figure class="m-0">
+                <blockquote class="blockquote m-0 fs-6">
+                    <p>
+                        Semoga website ini dapat membantu Gereja dalam
+                        mengelola data inventaris barang.
+                    </p>
+                </blockquote>
+                <figcaption class="blockquote-footer m-0">
+                    Kenny Jonathan Setiosaputro,
+                    Mahasiswa Teknik Infromatika Universitas Bina Nusantara Malang
+                </figcaption>
+            </figure>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4 col-xl-4 col-xxl-3 card mb-1" style="
+            background-image: url('../img/card-name.webp'); background-size: cover;">
+            <img src="../img/dimas.webp" class="img-custom p-2" alt="image">
+            <div class="card-body bg-white">
+            <figure class="m-0">
+                <blockquote class="blockquote m-0 fs-6">
+                    <p>
+                        Terima kasih atas semangat dan komitmen anak muda dalam
+                        memajukan gereja melalui pengembangan pastoral dan
+                        digitalisasi.
+                    </p>
+                </blockquote>
+                <figcaption class="blockquote-footer m-0">
+                    Damianus Dimas Agil Satrio Wibowo,
+                    Ketua Komunikasi Sosial Paroki
+                    Albertus Broadcast & Media (ARCADIA)
+                </figcaption>
+            </figure>
+            </div>
+        </div>
+    </div>
+</div>
